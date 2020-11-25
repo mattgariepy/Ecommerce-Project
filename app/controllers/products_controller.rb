fd1e152132e44d976cbff08ea2f5928b4cbda0be
Matthew Gariepy <mgariepy@aceproject.space>
2020-11-25 10:08:02 -0600

    Created a partial for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,12 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def onSale
+    @products = Product.includes(:category).where(onSale: [true, nil]).page(params[:page])
+  end
+
+  def new
+    @products = Product.includes(:category).where("created_at > ?", 3.days.ago).page(params[:page])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+  collection do
+    get 'onSale'
+    get 'new'
+  end
+end
   resources :categories, only: [:index, :show]
   root to: 'home#index'
 end
