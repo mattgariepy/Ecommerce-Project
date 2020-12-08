5f67d57b34fcc1bc5bba86f3ab529e22af0994c9
Matthew Gariepy <mgariepy@aceproject.space>
2020-12-07 21:21:03 -0600

    Users can now remove products from their shopping cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,12 +1,19 @@
 class CartController < ApplicationController
   def create
     id = params[:id].to_i
-    session[:shopping_cart] << id unless session[:shopping_cart].include?(id)
-    flash[:notice] = "Item added to cart!"
-    # redirect_to root_path
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      product = Product.find(id)
+      flash[:notice] = "#{product.name} added to cart!"
+      # redirect_to root_path
+    end
   end
 
   def destroy
-
+    id = params[:id].to_i
+    product = Product.find(id)
+    session[:shopping_cart].delete(id)
+    flash[:notice] = "#{product.name} removed from cart!"
+    # redirect_to root_path
   end
 end
